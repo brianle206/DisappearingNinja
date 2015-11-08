45e75d59c02b77baa313b1f8554cc32b42a4ace9
Brian Le <brianle206@gmail.com>
2015-11-08 14:37:07 -0800

    made change so use only 2 routes

--- server.py
+++ server.py
@@ -1,28 +1,28 @@
-from flask import Flask, redirect, render_template, request, send_file, url_for
+from flask import Flask, render_template, request, redirect
+
 app = Flask(__name__)
+
 @app.route('/')
 def index():
-	return render_template("index.html")
-@app.route('/ninja')
-def blue():
-	return render_template("ninjas.html")
+	return render_template('index.html')
 
-@app.route('/ninja/<ninja_color>')
-def donatello(ninja_color):
-	if ninja_color == 'blue':
-		print "blue"
-		return render_template("leonardo.html", ninja_color=ninja_color)
-	elif ninja_color == 'orange':
-		print "orange"
-		return render_template("michelangelo.html", ninja_color=ninja_color)
-	elif ninja_color == 'red':
-		print "red"
-		return render_template("ralphael.html", ninja_color=ninja_color)
-	elif ninja_color == 'purple':
-		print "purple"
-		return render_template("donatello.html", ninja_color=ninja_color)
+@app.route('/ninjas')
+def all():
+	filenames = ['leonardo.jpg', 'michelangelo.jpg', 'raphael.jpg', 'donatello.jpg']
+	return render_template('ninjas.html', filenames=filenames)
+
+@app.route('/ninja/<color>')
+def ninja(color):
+	if color == 'blue':
+		filenames = ['leonardo.jpg']
+	elif color == 'orange':
+		filenames = ['michelangelo.jpg']
+	elif color == 'red':
+		filenames = ['raphael.jpg']
+	elif color == 'purple':
+		filenames = ['donatello.jpg']
 	else:
-		print "anything else"
-		return render_template("april.html", ninja_color=ninja_color)
+		filenames = ['notapril.jpg']
+	return render_template('index.html', filenames=filenames)
 
 app.run(debug=True)
